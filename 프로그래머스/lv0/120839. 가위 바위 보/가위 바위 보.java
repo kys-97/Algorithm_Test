059b6824f3b78fe7525cc034a351497dd64d473d
kys-97 <115993353+kys-97@users.noreply.github.com>
2023-09-25 20:25:43 +0900

    [level 0] Title: 가위 바위 보, Time: 26.30 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        //가위 = 2, 바위 = 0, 보 = 5
+        for (int i = 0; i <rsp.length(); i++) {
+            if (rsp.split("")[i].equals("2")) {
+                answer += "0";
+            }
+            else if (rsp.split("")[i].equals("0")) {
+                answer += "5";
+            }
+            else {
+                answer += "2";
+            }
+        }
+        return answer;
+    }
+}
