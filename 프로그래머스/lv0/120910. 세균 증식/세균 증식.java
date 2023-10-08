e93bf46b97b538c91a7017ffadbef5dcbea8ce4f
kys-97 <115993353+kys-97@users.noreply.github.com>
2023-10-08 19:37:46 +0900

    [level 0] Title: 세균 증식, Time: 0.13 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-        for (int i = 0; i <t; i++) {
-            n *= 2;
+   //     int answer = 0;
+//         for (int i = 0; i <t; i++) {
+//             n *= 2;
             
-        }
-        answer = n;
+//         }
+//         answer = n;
+        
+        int answer = n*(int)Math.pow(2,t);
         return answer;
     }
 }
