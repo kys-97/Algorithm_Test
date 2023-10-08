bc9082f7e2b98a16c30d7090041beb94444634cf
kys-97 <115993353+kys-97@users.noreply.github.com>
2023-10-08 19:37:10 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.39 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
-        //1 2 7 10 11 -> 5개 중 중앙값 = 3번째 -> array[3] -> length/2+1
+        //1 2 7 10 11 -> 5개 중 중앙값 = 3번째 -> array[2] -> length/2
         //array 정렬
         Arrays.sort(array);
         return array[array.length/2];
