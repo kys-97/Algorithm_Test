c84415a60f634f248b6ffb23a8e44109707e291a
kys-97 <115993353+kys-97@users.noreply.github.com>
2023-10-08 19:40:46 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,8 @@
+def solution(my_string, num1, num2):
+    answer = ''
+
+    #my_string을 list로 형변환
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2],my_string[num1]
+    
+    return answer.join(my_string)
